870f6825134c99ab82957854b5199d723980be05
guranxp <guranxp@gmail.com>
2026-01-24 20:42:24 +0100

    Initial commit

---
+++ src/main/java/com/guranxp/trainingeventapp/TrainingEventAppApplication.java
@@ -0,0 +1,13 @@
+package com.guranxp.trainingeventapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingEventAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingEventAppApplication.class, args);
+	}
+
+}
