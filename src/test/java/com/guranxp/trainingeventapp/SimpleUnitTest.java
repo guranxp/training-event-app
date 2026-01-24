f3954ea09d5173e29dbbdd5a720e820ec9583674
guranxp <guranxp@gmail.com>
2026-01-24 23:07:40 +0100

    Added empty unit test.

---
+++ src/test/java/com/guranxp/trainingeventapp/SimpleUnitTest.java
@@ -0,0 +1,12 @@
+package com.guranxp.trainingeventapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleUnitTest {
+
+    @Test
+    void trueIsTrue() {
+        assertTrue(true);
+    }
+}
